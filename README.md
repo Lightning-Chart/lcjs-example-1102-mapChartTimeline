f6dd6663348200f40c21ebe66b19aa90f125dd61
Ilkka Kuosmanen <ilkka.kuosmanen@lightningchart.com>
2023-02-13 13:48:56 +0200

    LCJS 4.0.1

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # JavaScript Real-time Map Dashboard
 
-![JavaScript Real-time Map Dashboard](mapChartTimeline.png)
+![JavaScript Real-time Map Dashboard](mapChartTimeline-darkGold.png)
 
 This demo application belongs to the set of examples for LightningChart JS, data visualization library for JavaScript.
 
@@ -8,13 +8,13 @@ LightningChart JS is entirely GPU accelerated and performance optimized charting
 
 The demo can be used as an example or a seed project. Local execution requires the following steps:
 
-- Make sure that relevant version of [Node.js](https://nodejs.org/en/download/) is installed
-- Open the project folder in a terminal:
+-   Make sure that relevant version of [Node.js](https://nodejs.org/en/download/) is installed
+-   Open the project folder in a terminal:
 
-        npm install              # fetches dependencies
-        npm start                # builds an application and starts the development server
+          npm install              # fetches dependencies
+          npm start                # builds an application and starts the development server
 
-- The application is available at *http://localhost:8080* in your browser, webpack-dev-server provides hot reload functionality.
+-   The application is available at _http://localhost:8080_ in your browser, webpack-dev-server provides hot reload functionality.
 
 
 ## Description
@@ -31,24 +31,25 @@ Every time the active year is changed, map regions data is quickly invalidated i
 
 ```js
 // Code snippet setting region values for all 3 map charts.
-let year = 2005;
+let year = 2005
 
 const regionValuesData = populationData.map((item) => ({
-  ISO_A3: item["Country Code"],
-  value: item[year],
-}));
+    ISO_A3: item['Country Code'],
+    value: item[year],
+}))
 
-chartEurope.invalidateRegionValues(regionValuesData);
-chartSA.invalidateRegionValues(regionValuesData);
-chartNA.invalidateRegionValues(regionValuesData);
+chartEurope.invalidateRegionValues(regionValuesData)
+chartSA.invalidateRegionValues(regionValuesData)
+chartNA.invalidateRegionValues(regionValuesData)
 ```
 
 **More map examples**:
 
-- [Data visualization over LightningChart JS Map Charts](https://www.arction.com/lightningchart-js-interactive-examples/examples/lcjs-example-1103-mapChartVizXY.html)
-- [Data visualization over Google Maps](https://blog.arction.com/easy-geospatial-data-visualization-with-lightningchart-js-and-google)
-- [LightningChart JS Map Charts with Drill-down](https://www.arction.com/lightningchart-js-interactive-examples/examples/lcjs-example-1111-covidDrillDownDashboard.html)
-- [Data visualization over static map picture](https://www.arction.com/lightningchart-js-interactive-examples/examples/lcjs-example-1110-geoChartUsaTemperature.html)
+-   [Data visualization over LightningChart JS Map Charts](https://lightningchart.com/lightningchart-js-interactive-examples/examples/lcjs-example-1103-mapChartVizXY.html)
+-   [Data visualization over Google Maps](https://blog.arction.com/easy-geospatial-data-visualization-with-lightningchart-js-and-google)
+-   [LightningChart JS Map Charts with Drill-down](https://lightningchart.com/lightningchart-js-interactive-examples/examples/lcjs-example-1111-covidDrillDownDashboard.html)
+-   [Data visualization over static map picture](https://lightningchart.com/lightningchart-js-interactive-examples/examples/lcjs-example-1110-geoChartUsaTemperature.html)
+
 
 ## API Links
 
@@ -67,30 +68,30 @@ chartNA.invalidateRegionValues(regionValuesData);
 
 If you notice an error in the example code, please open an issue on [GitHub][0] repository of the entire example.
 
-Official [API documentation][1] can be found on [Arction][2] website.
+Official [API documentation][1] can be found on [LightningChart][2] website.
 
 If the docs and other materials do not solve your problem as well as implementation help is needed, ask on [StackOverflow][3] (tagged lightningchart).
 
-If you think you found a bug in the LightningChart JavaScript library, please contact support@arction.com.
+If you think you found a bug in the LightningChart JavaScript library, please contact support@lightningchart.com.
 
-Direct developer email support can be purchased through a [Support Plan][4] or by contacting sales@arction.com.
+Direct developer email support can be purchased through a [Support Plan][4] or by contacting sales@lightningchart.com.
 
 [0]: https://github.com/Arction/
-[1]: https://www.arction.com/lightningchart-js-api-documentation/
-[2]: https://www.arction.com
+[1]: https://lightningchart.com/lightningchart-js-api-documentation/
+[2]: https://lightningchart.com
 [3]: https://stackoverflow.com/questions/tagged/lightningchart
-[4]: https://www.arction.com/support-services/
+[4]: https://lightningchart.com/support-services/
 
-© Arction Ltd 2009-2020. All rights reserved.
+© LightningChart Ltd 2009-2022. All rights reserved.
 
 
-[Dashboard]: https://www.arction.com/lightningchart-js-api-documentation/v3.4.0/classes/dashboard.html
-[Map chart]: https://www.arction.com/lightningchart-js-api-documentation/v3.4.0/classes/mapchart.html
-[Map types]: https://www.arction.com/lightningchart-js-api-documentation/v3.4.0/globals.html#maptypes
-[Color look up table]: https://www.arction.com/lightningchart-js-api-documentation/v3.4.0/classes/lut.html
-[Paletted fill style]: https://www.arction.com/lightningchart-js-api-documentation/v3.4.0/classes/palettedfill.html
-[Color factory RGBA]: https://www.arction.com/lightningchart-js-api-documentation/v3.4.0/globals.html#colorrgba
-[UI element builders]: https://www.arction.com/lightningchart-js-api-documentation/v3.4.0/globals.html#uielementbuilders
-[UI layout builders]: https://www.arction.com/lightningchart-js-api-documentation/v3.4.0/globals.html#uilayoutbuilders
-[Legend box builders]: https://www.arction.com/lightningchart-js-api-documentation/v3.4.0/globals.html#legendboxbuilders
+[Dashboard]: https://lightningchart.com/lightningchart-js-api-documentation/v4.0.0/classes/Dashboard.html
+[Map chart]: https://lightningchart.com/lightningchart-js-api-documentation/v4.0.0/classes/MapChart.html
+[Map types]: https://lightningchart.com/lightningchart-js-api-documentation/v4.0.0/variables/MapTypes.html
+[Color look up table]: https://lightningchart.com/lightningchart-js-api-documentation/v4.0.0/classes/LUT.html
+[Paletted fill style]: https://lightningchart.com/lightningchart-js-api-documentation/v4.0.0/classes/PalettedFill.html
+[Color factory RGBA]: https://lightningchart.com/lightningchart-js-api-documentation/v4.0.0/functions/ColorRGBA.html
+[UI element builders]: https://lightningchart.com/lightningchart-js-api-documentation/v4.0.0/variables/UIElementBuilders.html
+[UI layout builders]: https://lightningchart.com/lightningchart-js-api-documentation/v4.0.0/variables/UILayoutBuilders.html
+[Legend box builders]: https://lightningchart.com/lightningchart-js-api-documentation/v4.0.0/variables/LegendBoxBuilders.html
 
--- src/index.js
+++ src/index.js
@@ -2,302 +2,259 @@
  * LightningChart JS Example on using `MapChart` with real-time animated data set and dynamic region coloring.
  */
 // Import LightningChartJS
-const lcjs = require("@arction/lcjs");
+const lcjs = require('@arction/lcjs')
 
 // Extract required parts from LightningChartJS.
 const {
-  lightningChart,
-  MapTypes,
-  PalettedFill,
-  LUT,
-  ColorRGBA,
-  emptyFill,
-  emptyLine,
-  UIOrigins,
-  LegendBoxBuilders,
-  UIElementBuilders,
-  UILayoutBuilders,
-  Themes,
-} = lcjs;
-
-// Specify color look up table for coloring countries. Color depends on % of population change
-const palette = new PalettedFill({
-  lut: new LUT({
-    steps: [
-      {
-        value: -5,
-        color: ColorRGBA(0, 0, 100),
-        label: "-5%",
-      },
-      {
-        value: -2.5,
-        color: ColorRGBA(0, 0, 255),
-        label: "-2.5%",
-      },
-      {
-        value: 0,
-        color: ColorRGBA(200, 200, 200),
-        label: "0%",
-      },
-      {
-        value: 2.5,
-        color: ColorRGBA(255, 0, 0),
-        label: "+2.5%",
-      },
-      {
-        value: 5,
-        color: ColorRGBA(100, 0, 0),
-        label: "+5%",
-      },
-    ],
-    interpolate: true,
-    // This property is used to specify fallback color for regions which have no data.
-    color: ColorRGBA(255, 255, 255),
-  }),
-});
+    lightningChart,
+    MapTypes,
+    PalettedFill,
+    LUT,
+    ColorRGBA,
+    emptyFill,
+    emptyLine,
+    UIOrigins,
+    LegendBoxBuilders,
+    UIElementBuilders,
+    UILayoutBuilders,
+    Themes,
+} = lcjs
 
 // Create Dashboard and Charts.
 const dashboard = lightningChart().Dashboard({
-  // theme: Themes.darkGold
-  numberOfColumns: 3,
-  numberOfRows: 2,
-});
+    // theme: Themes.darkGold
+    numberOfColumns: 3,
+    numberOfRows: 2,
+})
+const theme = dashboard.getTheme()
+
+// Specify color look up table for coloring countries. Color depends on % of population change
+const palette = new PalettedFill({
+    lut: new LUT({
+        steps: [
+            {
+                value: -5,
+                color: theme.examples.badGoodColorPalette[theme.examples.badGoodColorPalette.length - 1],
+                label: '-5%',
+            },
+            {
+                value: 0,
+                color: ColorRGBA(200, 200, 200),
+                label: '0%',
+            },
+            {
+                value: 5,
+                color: theme.examples.badGoodColorPalette[0],
+                label: '+5%',
+            },
+        ],
+        interpolate: true,
+        // This property is used to specify fallback color for regions which have no data.
+        color: ColorRGBA(255, 255, 255),
+    }),
+})
 
 // Specify cursor result table formatter for all map charts.
-const cursorResultTableFormatter = (
-  builder,
-  region,
-  value,
-  longitude,
-  latitude
-) => {
-  builder
-    .addRow("", region.name, "")
-    .addRow("", `Population change ${currentYear}`, "");
-  if (value) {
-    builder.addRow("", `${value > 0 ? "+" : ""}${value.toFixed(2)} %`, "");
-  }
-  return builder;
-};
+const cursorResultTableFormatter = (builder, region, value, longitude, latitude) => {
+    builder.addRow('', region.name, '').addRow('', `Population change ${currentYear}`, '')
+    if (value) {
+        builder.addRow('', `${value > 0 ? '+' : ''}${value.toFixed(2)} %`, '')
+    }
+    return builder
+}
 
 // North America region
 const chartNA = dashboard
-  .createMapChart({
-    type: MapTypes.NorthAmerica,
-    columnIndex: 0,
-    rowIndex: 0,
-    columnSpan: 1,
-  })
-  .setTitle("North America")
-  .setFillStyle(palette)
-  .setCursorResultTableFormatter(cursorResultTableFormatter);
+    .createMapChart({
+        type: MapTypes.NorthAmerica,
+        columnIndex: 0,
+        rowIndex: 0,
+        columnSpan: 1,
+    })
+    .setTitle('North America')
+    .setFillStyle(palette)
+    .setCursorResultTableFormatter(cursorResultTableFormatter)
 
 // South America region
 const chartSA = dashboard
-  .createMapChart({
-    type: MapTypes.SouthAmerica,
-    columnIndex: 1,
-    rowIndex: 0,
-    columnSpan: 1,
-  })
-  .setTitle("South America")
-  .setFillStyle(palette)
-  .setCursorResultTableFormatter(cursorResultTableFormatter);
+    .createMapChart({
+        type: MapTypes.SouthAmerica,
+        columnIndex: 1,
+        rowIndex: 0,
+        columnSpan: 1,
+    })
+    .setTitle('South America')
+    .setFillStyle(palette)
+    .setCursorResultTableFormatter(cursorResultTableFormatter)
 
 // Europe region
 const chartEurope = dashboard
-  .createMapChart({
-    type: MapTypes.Europe,
-    columnIndex: 0,
-    rowIndex: 1,
-    columnSpan: 3,
-    rowSpan: 1
-  })
-  .setTitle("Europe")
-  .setFillStyle(palette)
-  .setCursorResultTableFormatter(cursorResultTableFormatter);
+    .createMapChart({
+        type: MapTypes.Europe,
+        columnIndex: 0,
+        rowIndex: 1,
+        columnSpan: 3,
+        rowSpan: 1,
+    })
+    .setTitle('Europe')
+    .setFillStyle(palette)
+    .setCursorResultTableFormatter(cursorResultTableFormatter)
 
 // Create UI panel inside Dashboard for placing legend and some extra controls.
 const panel = dashboard.createUIPanel({
-  columnIndex: 2,
-  rowIndex: 0,
-  columnSpan: 1,
-  rowSpan: 1
-});
+    columnIndex: 2,
+    rowIndex: 0,
+    columnSpan: 1,
+    rowSpan: 1,
+})
 
 panel.onResize((chart, width, height, engineWidth, engineHeight) => {
-  block.style.width = width  + 'px'
-  block.style.left = engineWidth - width + 'px'
+    block.style.width = width + 'px'
+    block.style.left = engineWidth - width + 'px'
 })
 chartEurope.onResize((chart, width, height, engineWidth, engineHeight) => {
-  block.style.top = (engineHeight - height ) - 82 +'px'
+    block.style.top = engineHeight - height - 82 + 'px'
 })
 
 // NOTE: Custom Legend Box is created with UI elements.
 const legendLayout = panel
-  .addUIElement(UILayoutBuilders.Column)
-  .setPosition({
-    x: 0,
-    y: 20,
-  })
-  .setOrigin(UIOrigins.LeftBottom)
-  .setMargin(6)
-  .setMouseInteractions(false)
-  .setBackground(bg => bg
-      .setFillStyle(emptyFill)
-      .setStrokeStyle(emptyLine)
-  )
+    .addUIElement(UILayoutBuilders.Column)
+    .setPosition({
+        x: 0,
+        y: 20,
+    })
+    .setOrigin(UIOrigins.LeftBottom)
+    .setMargin(6)
+    .setMouseInteractions(false)
+    .setBackground((bg) => bg.setFillStyle(emptyFill).setStrokeStyle(emptyLine))
 
-const legendTitle = legendLayout
-  .addElement(UIElementBuilders.TextBox)
-  .setText("Population change (% / year)")
+const legendTitle = legendLayout.addElement(UIElementBuilders.TextBox).setText('Population change (% / year)')
 
-const legendBuilder = LegendBoxBuilders.HorizontalLegendBox.addStyler(
-  (legendBox) =>
-    legendBox
-      .setBackground((background) =>
-        background.setFillStyle(emptyFill).setStrokeStyle(emptyLine)
-      )
-      .setTitle("")
-);
+const legendBuilder = LegendBoxBuilders.HorizontalLegendBox.addStyler((legendBox) =>
+    legendBox.setBackground((background) => background.setFillStyle(emptyFill).setStrokeStyle(emptyLine)).setTitle(''),
+)
 
-const legendNA = legendLayout.addElement(legendBuilder).add(chartNA);
-const legendSA = legendLayout.addElement(legendBuilder).add(chartSA);
-const legendEurope = legendLayout.addElement(legendBuilder).add(chartEurope);
+const legendNA = legendLayout.addElement(legendBuilder).add(chartNA)
+const legendSA = legendLayout.addElement(legendBuilder).add(chartSA)
+const legendEurope = legendLayout.addElement(legendBuilder).add(chartEurope)
 
 // Add dynamically injected HTML elements for active Year slider.
-const yearDiv = document.createElement("div");
-yearDiv.id = "block";
-const yearSlider = document.createElement("input");
-yearSlider.type = "range";
-yearSlider.min = 1961;
-yearSlider.max = 2018;
-yearSlider.value = yearSlider.min;
-yearSlider.id = "slider";
-const yearMarker = document.createElement("span");
-yearMarker.id = "marker";
-yearDiv.appendChild(yearMarker);
-yearDiv.appendChild(yearSlider);
-dashboard.engine.container.append(yearDiv);
+const yearDiv = document.createElement('div')
+yearDiv.id = 'block'
+const yearSlider = document.createElement('input')
+yearSlider.type = 'range'
+yearSlider.min = 1961
+yearSlider.max = 2018
+yearSlider.value = yearSlider.min
+yearSlider.id = 'slider'
+const yearMarker = document.createElement('span')
+yearMarker.id = 'marker'
+yearDiv.appendChild(yearMarker)
+yearDiv.appendChild(yearSlider)
+dashboard.engine.container.append(yearDiv)
 
-const markerColorGreen = "rgb(0, 0, 200, 0.6)";
-const markerColorDarkGreen = "rgb(0, 0, 150)";
-const markerColorRed = "rgb(200, 0, 0)";
-const markerColorDarkRed = "rgb(150, 0, 0)";
+const markerColorGreen = 'rgb(0, 0, 200, 0.6)'
+const markerColorDarkGreen = 'rgb(0, 0, 150)'
+const markerColorRed = 'rgb(200, 0, 0)'
+const markerColorDarkRed = 'rgb(150, 0, 0)'
 
 // Keep track of animation properties.
-let currentYear = yearSlider.min;
-let yearAnimationActive = true;
-let interval;
+let currentYear = yearSlider.min
+let yearAnimationActive = true
+let interval
 
 // Fetch data set used in example.
-fetch(
-  document.head.baseURI +
-    "examples/assets/1102/population60-21.json"
-)
-  .then((r) => r.json())
-  .then((populationData) => {
-    // stop timeline, change the year  and change bg color of slider by clicking on slider
-    yearSlider.oninput = () => {
-      currentYear = yearSlider.value;
-      clearInterval(interval);
-      yearAnimationActive = false;
-      yearMarker.style.background = markerColorRed;
-      setMap(populationData, currentYear);
-      const value =
-        ((yearSlider.value - yearSlider.min) * 100) /
-        (yearSlider.max - yearSlider.min);
-      yearSlider.style.background =
-        "linear-gradient(to right, rgba(0,0,255,.6)" +
-        value +
-        "%, #fff " +
-        value +
-        "%, white 100%)";
-    };
-
-    // change the color of the marker on hover
-    yearMarker.onmouseover = () => {
-      if (yearAnimationActive) {
-        yearMarker.style.background = markerColorDarkRed;
-      } else {
-        yearMarker.style.background = markerColorDarkGreen;
-      }
-    };
-
-    // reset the color of the marker on mouse out
-    yearMarker.onmouseout = () => {
-      if (yearAnimationActive) {
-        yearMarker.style.background = markerColorGreen;
-      } else {
-        yearMarker.style.background = markerColorRed;
-      }
-    };
-
-    // pause/start timeline by clicking the marker
-    yearMarker.onclick = () => {
-      if (yearAnimationActive) {
-        clearInterval(interval);
-        yearAnimationActive = false;
-        yearMarker.style.background = markerColorRed;
-      } else {
-        intervalHandler();
-        yearAnimationActive = true;
-        yearMarker.style.background = markerColorGreen;
-      }
-    };
-
-    // set timeline
-    const intervalHandler = () => {
-      // subscribe var. 'interval' to 'setInterval'
-      interval = setInterval(() => {
-        // Map data to format expected by MapChart.
-        setMap(populationData, currentYear);
-        // if timeline is over wait for 2sec and start it again
-        if (currentYear >= 2019 && yearAnimationActive) {
-          setTimeout(() => {
-            currentYear = yearSlider.min;
-          }, 1800);
-        } else {
-          yearSlider.value = currentYear;
-          // change BG of slider
-          const value =
-            ((yearSlider.value - yearSlider.min) * 100) /
-            (yearSlider.max - yearSlider.min);
-          yearSlider.style.background =
-            "linear-gradient(to right, rgba(0,0,255,.6)" +
-            value +
-            "%, #fff " +
-            value +
-            "%, white 100%)";
-          currentYear++;
+fetch(document.head.baseURI + 'examples/assets/1102/population60-21.json')
+    .then((r) => r.json())
+    .then((populationData) => {
+        // stop timeline, change the year  and change bg color of slider by clicking on slider
+        yearSlider.oninput = () => {
+            currentYear = yearSlider.value
+            clearInterval(interval)
+            yearAnimationActive = false
+            yearMarker.style.background = markerColorRed
+            setMap(populationData, currentYear)
+            const value = ((yearSlider.value - yearSlider.min) * 100) / (yearSlider.max - yearSlider.min)
+            yearSlider.style.background = 'linear-gradient(to right, rgba(0,0,255,.6)' + value + '%, #fff ' + value + '%, white 100%)'
         }
-      }, 200);
-    };
 
-    intervalHandler();
+        // change the color of the marker on hover
+        yearMarker.onmouseover = () => {
+            if (yearAnimationActive) {
+                yearMarker.style.background = markerColorDarkRed
+            } else {
+                yearMarker.style.background = markerColorDarkGreen
+            }
+        }
 
-    const setMap = (populationData, year) => {
-      // set year for marker
-      yearMarker.textContent = year;
+        // reset the color of the marker on mouse out
+        yearMarker.onmouseout = () => {
+            if (yearAnimationActive) {
+                yearMarker.style.background = markerColorGreen
+            } else {
+                yearMarker.style.background = markerColorRed
+            }
+        }
 
-      // calculate population change from previous year and put into map chart data format.
-      const regionValuesData = populationData.map((item) => ({
-        ISO_A3: item["Country Code"],
-        value: ((item[year] - item[year - 1]) * 100) / item[year],
-      }));
+        // pause/start timeline by clicking the marker
+        yearMarker.onclick = () => {
+            if (yearAnimationActive) {
+                clearInterval(interval)
+                yearAnimationActive = false
+                yearMarker.style.background = markerColorRed
+            } else {
+                intervalHandler()
+                yearAnimationActive = true
+                yearMarker.style.background = markerColorGreen
+            }
+        }
 
-      // update map chart region values
-      chartEurope.invalidateRegionValues(regionValuesData);
-      chartSA.invalidateRegionValues(regionValuesData);
-      chartNA.invalidateRegionValues(regionValuesData);
-    };
-  });
+        // set timeline
+        const intervalHandler = () => {
+            // subscribe var. 'interval' to 'setInterval'
+            interval = setInterval(() => {
+                // Map data to format expected by MapChart.
+                setMap(populationData, currentYear)
+                // if timeline is over wait for 2sec and start it again
+                if (currentYear >= 2019 && yearAnimationActive) {
+                    setTimeout(() => {
+                        currentYear = yearSlider.min
+                    }, 1800)
+                } else {
+                    yearSlider.value = currentYear
+                    // change BG of slider
+                    const value = ((yearSlider.value - yearSlider.min) * 100) / (yearSlider.max - yearSlider.min)
+                    yearSlider.style.background =
+                        'linear-gradient(to right, rgba(0,0,255,.6)' + value + '%, #fff ' + value + '%, white 100%)'
+                    currentYear++
+                }
+            }, 200)
+        }
+
+        intervalHandler()
+
+        const setMap = (populationData, year) => {
+            // set year for marker
+            yearMarker.textContent = year
+
+            // calculate population change from previous year and put into map chart data format.
+            const regionValuesData = populationData.map((item) => ({
+                ISO_A3: item['Country Code'],
+                value: ((item[year] - item[year - 1]) * 100) / item[year],
+            }))
+
+            // update map chart region values
+            chartEurope.invalidateRegionValues(regionValuesData)
+            chartSA.invalidateRegionValues(regionValuesData)
+            chartNA.invalidateRegionValues(regionValuesData)
+        }
+    })
 
 // Dynamically inject some CSS to example.
 function addStyle(styleString) {
-  const style = document.createElement("style");
-  style.textContent = styleString;
-  document.head.append(style);
+    const style = document.createElement('style')
+    style.textContent = styleString
+    document.head.append(style)
 }
 
 addStyle(`
@@ -371,4 +328,4 @@ addStyle(`
     transform: scale(0.9);
   }
 
-  `);
+  `)
